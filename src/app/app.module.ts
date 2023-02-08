4e88784bcb5b8afc58caebbc698d90f9f2687cd0
PandoraArc <parinthon.nea@gmail.com>
2023-02-08 10:26:25 +0700

    implement new widget

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WidgetActionBarComponent } from './widgets/widget-action-bar/widget-action-bar.component';
+import { WidgetTextBoxComponent } from './widgets/widget-text-box/widget-text-box.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WidgetActionBarComponent
+    WidgetActionBarComponent,
+    WidgetTextBoxComponent
   ],
   imports: [
     BrowserModule,
