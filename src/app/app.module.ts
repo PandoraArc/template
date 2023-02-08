8ddf3bb1077739e66ea44ad4ac39808390981bc7
PandoraArc <parinthon.nea@gmail.com>
2023-02-08 09:46:00 +0700

    add action bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WidgetActionBarComponent } from './widgets/widget-action-bar/widget-action-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WidgetActionBarComponent
   ],
   imports: [
     BrowserModule,
